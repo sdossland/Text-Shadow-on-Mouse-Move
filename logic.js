b159004a8337ed8356eaaa85fbde3eba6b94bcaf
Sophia Dossland <sophia.dossland@gmail.com>
2017-04-14 16:19:15 -0700

    initiate text-shadow move on mousemove event

--- logic.js
+++ logic.js
@@ -3,10 +3,33 @@
  */
 document.addEventListener("DOMContentLoaded", function () {
 
-  function moveShadow() {
+  const container = document.querySelector('.container');
+  const text = container.querySelector('p');
+  const move = 60;
 
+  function moveShadow(e) {
+    //console.log(e); //gives us x and y coords
+    const width = container.offsetWidth; //width of container, in this case of the viewport
+    const height = container.offsetHeight; //height of container, in this case of the viewport
+
+    //mouse position relative to target element
+    let x = e.offsetX;
+    let y = e.offsetY;
+    //'this' returns HTML element event is being called on
+    //'e.target' returns HTML element of children if mouse is over them, in this case p tag
+    //we could also just use 'x = e.pageX' in this example since we are using the entire page,
+    //but in this scenario we only want it to move while w/in a certain <div>, etc. can do this:
+    if (this !== e.target) {
+      x = x + e.target.offsetLeft; //adds px left of <p>
+      y = y + e.target.offsetTop; //adds px above <p>
+    }
+    //can move a total of 60px. -30 left, +30 right
+    const moveX = Math.round(x / width * move - move / 2);
+    const moveY = Math.round(y / height * move - move / 2);
+
+    text.style.textShadow = `${moveX}px ${moveY}px 2px #000`;
   }
 
-  document.addEventListener('onmouseover', moveShadow);
+  container.addEventListener('mousemove', moveShadow);
 
 });
