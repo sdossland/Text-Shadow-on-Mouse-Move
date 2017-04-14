c20a1433f74e76e3d07bf1aba2dbc568d225f633
Sophia Dossland <sophia.dossland@gmail.com>
2017-04-13 17:41:00 -0700

    initial commit

---
+++ logic.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sophia on 4/13/17.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+
+  function moveShadow() {
+
+  }
+
+  document.addEventListener('onmouseover', moveShadow);
+
+});
